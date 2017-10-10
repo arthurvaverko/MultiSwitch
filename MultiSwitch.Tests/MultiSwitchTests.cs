1fc58bb1cf9bdb673eb480a822f67108abdfd600
Arthur Vaverko <Arthur.Vaverko@kaltura.com>
2017-10-10 11:09:14 +0300

    initial code commit

---
+++ MultiSwitch.Tests/MultiSwitchTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using MultiSwitch;
+
+namespace MultiSwitch.Tests
+{
+    [TestFixture]
+    public class MultiSwitchTests
+    {
+        [Test]
+        public void ShouldFindOneMatch()
+        {
+            var correctMatchWasExecuted = false;
+            const int i = 1;
+            const int j = 34;
+            const bool k = true;
+            MultiSwitch.Match(i, j, k).
+                With(1, 2, false).Do(() => Console.WriteLine("1, 2, 3")).
+                With(1, 34, false).Do(() => Console.WriteLine("1, 34, false")).
+                With(1, 34, true).Do(() =>
+                {
+                    Console.WriteLine("1, 34, true");
+                    correctMatchWasExecuted = true;
+                });
+
+            Assert.That(correctMatchWasExecuted);
+
+        }
+
+        [Test]
+        public void ShouldExecuteDefault()
+        {
+            var defaultWasExecuted = false;
+
+            const int i = 1;
+            const int j = 34;
+            const bool k = true;
+            MultiSwitch.Match(i, j, k).
+                With(1, 2, false).Do(() => Console.WriteLine("1, 2, 3")).
+                With(1, 34, false).Do(() => Console.WriteLine("1, 34, false")).
+                Default(() =>
+                {
+                    Console.WriteLine("This is default action if not match found");
+                    defaultWasExecuted = true;
+                });
+
+            Assert.That(defaultWasExecuted);
+        }
+    }
+}
---
+++ MultiSwitch/MultiSwitch.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiSwitch
+{
+    public class MultiSwitch
+    {
+        public object[] Values { get; }
+        private bool _AtLeastOneMatchFound = false;
+
+
+        public MultiSwitch(params object[] values)
+        {
+            Values = values;
+        }
+
+        public PossibleMatch With(params object[] options)
+        {
+            if (options.Length != Values.Length) { throw new ArgumentException("Too many options provided, Length of options must match pattern length"); }
+
+            var possibleMatch = new PossibleMatch(this, options);
+            if (possibleMatch.IsMatch) { _AtLeastOneMatchFound = true; }
+            return possibleMatch;
+        }
+
+        public void Default(Action actionToInvokeIfMatch)
+        {
+            if (!_AtLeastOneMatchFound)
+            {
+                actionToInvokeIfMatch();
+            }
+        }
+
+        public static MultiSwitch Match(params object[] values)
+        {
+            return new MultiSwitch(values);
+        }
+
+        public class PossibleMatch
+        {
+            private readonly MultiSwitch _MultiSwitch;
+            private readonly object[] _MatchedValues;
+            public bool IsMatch { get; private set; }
+
+            public PossibleMatch(MultiSwitch matcher, object[] valuesToTestForMatch)
+            {
+                _MultiSwitch = matcher;
+                _MatchedValues = valuesToTestForMatch;
+            }
+
+            public MultiSwitch Do(Action actionToInvokeIfMatch)
+            {
+                if (_MultiSwitch.Values.SequenceEqual(_MatchedValues))
+                {
+                    IsMatch = true;
+                    actionToInvokeIfMatch();
+                }
+
+                return _MultiSwitch;
+            }
+        }
+    }
+
+
+}
